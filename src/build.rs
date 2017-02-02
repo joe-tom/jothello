7d19d5fd8cd14355cecdea23c16710f9f71705e8
Joseph Thomas <joey6978@gmail.com>
2017-02-02 13:06:55 -0800

    Commenting build, have to finish move generation

--- src/build.rs
+++ src/build.rs
@@ -1,8 +1,14 @@
 
 use std::fmt;
 
-static TRANSFORM: [u32; 6561] = [0; 6561];
+// Enumeration for stone colour type, in array lookup.
+static WHITE: usize = 0;
+static BLACK: usize = 1;
 
+// Consider adding an array specifically for diagonals.
+static TRANSFORM: [[[u32; 8]; 6561] = [[0; 8]; 6561];
+
+// Lookup table for binary to ternary transfromation.
 static BT: [u32; 256] = [0; 256];
 static BT2: [u32; 256] = [0; 256];
 
@@ -19,13 +25,23 @@ fn build_bin_to_trin () {
 }
 
 fn build_transform () {
+  // Iterate through every combination of white and black stones.
   for black in 0..256 {
     for white in 0..256 {
 
-      // Go through each square
-      for start in 0..8 {
-        num | (1 << start);
+      // Ignore invalid positions. i.e. intersections within stones.
+      if white&black {
+        continue;
+      }
 
+
+      // Duplicate white and black.
+      let new_black = black;
+      let new_white = white;
+
+      // Go through each square.
+      for start in 0..8 {
+        if new_white >> start & 1
       }
     }
   }
