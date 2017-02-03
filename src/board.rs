f66fe962838dce8da24fc034b6ee7e7f797f628b
Joseph Thomas <joey6978@gmail.com>
2017-02-03 09:42:25 -0800

    Gotta add undo move

--- src/board.rs
+++ src/board.rs
@@ -1,17 +1,20 @@
-
+static TRANSFORM: [[u16; 16]; 65536] = [[0; 16]; 65536];
+static UNDO_TRANSFORM: [[u16; 16]; 65536] = [[0; 16]; 65536];
 
 #[derive(Debug)]
 struct Board {
-  horiz: [u16; 8]
+  horiz: [u16; 8],
+  verti: [u16; 8],
+  diagr: [u16; 8],
+  diagl: [u16; 8]
 };
 
 impl Board {
   fn place_piece (&mut self, location: usize) {
-    let mov_h = HORIZ[location];
-    let mov_v = VERTI[location];
-    let mov_r = DIAGR[location];
-    let mov_l = DIAGL[location];
+    let mov_h: u8 = (location / 8);
+    let mov_v: u8 = (location % 8);
 
-    self.horiz[mov_h] = TRANSFORM[self.horiz[mov_h]][location][piece];
+    self.horiz[mov_h] = TRANSFORM[self.horiz[mov_h]][location];
   }
 }
+
